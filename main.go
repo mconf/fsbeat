cb71665292c52e2514221033a8211a000867cb2f
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2017-03-13 17:43:00 -0300

    First commit. Fresh new Beat.

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"github.com/elastic/beats/libbeat/beat"
+
+	"github.com/kmyokoyama/fsbeat/beater"
+)
+
+func main() {
+	err := beat.Run("fsbeat", "", beater.New)
+	if err != nil {
+		os.Exit(1)
+	}
+}
