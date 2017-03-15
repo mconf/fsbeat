eebf13f8ca6733781177a6faef471b1ef0877dc2
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2017-03-15 18:30:41 -0300

    Configuration source for version 1.0.

--- config/config.go
+++ config/config.go
@@ -7,8 +7,16 @@ import "time"
 
 type Config struct {
 	Period time.Duration `config:"period"`
+  MaxBuffer int `config:"max.buffer"`
+  FSServer string `config:"freeswitch.server"`
+  FSPort string `config:"freeswitch.port"`
+  FSAuth string `config:"freeswitch.auth"`
 }
 
 var DefaultConfig = Config{
 	Period: 1 * time.Second,
+  MaxBuffer: 20,
+  FSServer: "localhost",
+  FSPort: "8021",
+  FSAuth: "ClueCon",
 }
